a268affd6ebcc61eaa291d5dbff49e21792c8c24
ripper <officialpkkmkb@gmail.com>
2021-03-31 16:07:04 +0530

    Update conf.js

--- conf.js
+++ conf.js
@@ -1,89 +1,91 @@
 jsproxy_config({
-    // The current configuration version (recorded in the log for troubleshooting)
-    // Each time the configuration is modified, this value needs to be increased, otherwise it will not take effect.
-    // By default, the configuration is automatically downloaded every 5 minutes. If you want to verify immediately, you can access it in privacy mode.
-    ver: "110",
+  // 当前配置的版本（记录在日志中，用于排查问题）
+  // 每次修改配置，该值需要增加，否则不会生效。
+  // 默认每隔 5 分钟自动下载配置，若想立即验证，可通过隐私模式访问。
+  ver: '118',
 
-    // Accelerate static resources of commonly used websites through CDN (in experiment)
-    static_boost: {
-        enable: true,
-        ver: 62,
+  // 通过 CDN 加速常用网站的静态资源（实验中）
+  static_boost: {
+    enable: true,
+    ver: 79
+  },
+
+  // 节点配置
+  node_map: {
+    'demo-hk': {
+      label: '演示服务-香港节点',
+      lines: {
+        // 主机:权重
+        'node-aliyun-hk-0.etherdream.com:8443': 1,
+        'node-aliyun-hk-1.etherdream.com:8443': 1,
+        'node-aliyun-hk-2.etherdream.com:8443': 1,
+      }
     },
-
-    // Node configuration not configured ccorrectly
-    node_map: {
-        "demo-hk": {
-            label: "HONGKONG PROXY",
-            lines: {
-                "b.hehe.workers.dev:8443": 1, // Host: weight
-                "b.lulu.workers.dev:8443": 2,
-                "b.jsproxy.workers.dev:8443": 3,
-            },
-        },
-        "demo-sg": {
-            label: "SINGAPORE PROXY",
-            lines: {
-                "b.007.workers.dev:8443": 1,
-            },
-        },
-        mysite: {
-            label: "AUTO PROXY",
-            lines: {
-                [location.host]: 1,
-            },
-        },
-        cfworker: {
-            label: "",
-            hidden: true,
-            lines: {
-                // Paid version (high weight）
-                //'node-cfworker-2.etherdream.com': 4,
-                // Free version (low weight, share some costs)
-                //'b.007.workers.dev': 1,
-                //'b.hehe.workers.dev': 1,
-                //'b.lulu.workers.dev': 1,
-                //'b.jsproxy.workers.dev': 1,
-            },
-        },
+    'demo-sg': {
+      label: '演示服务-新加坡节点',
+      lines: {
+        'node-aliyun-sg.etherdream.com:8443': 1,
+      },
     },
-
-    /**
-     * Default node
-     */
-    node_default: "mysite",
-    // node_default: /jsproxy-demo\.\w+$/.test(location.host) ? 'demo-hk' : 'mysite',
-
-    /**
-     * Acceleration node
-     */
-    node_acc: "cfworker",
-
-    /**
-     * Static resource CDN address
-     * Used to accelerate access to resources in the `assets` directory
-     */
-    // assets_cdn: 'https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io@master/assets/',
-
-    // Open during local testing, otherwise access is online
-    assets_cdn: "assets/",
-
-    // Home Path
-    index_path: "index_v3.html",
-
-    // List of sites that support CORS (in experiment ...)
-    direct_host_list: "cors_v1.txt",
-
-    /**
-     * Customize the HTML injected into the page
-     */
-    inject_html: "<!-- custom html -->",
-
-    /**
-     * URL custom processing (under design)
-     */
-    url_handler: {
-        "http://haha.com/": {
-            content: "Hello World",
-        },
+    'mysite': {
+      label: '当前站点',
+      lines: {
+        [location.host]: 1,
+      }
     },
-});
+    // 该节点用于加载大体积的静态资源
+    'cfworker': {
+      label: '',
+      hidden: true,
+      lines: {
+        'node-cfworker-2.etherdream.com': 1,
+      }
+    }
+  },
+
+  /**
+   * 默认节点
+   */
+  // node_default: 'mysite',
+  node_default: /github\.io$/.test(location.host) ? 'demo-hk' : 'mysite',
+
+  /**
+   * 加速节点
+   */
+  node_acc: 'cfworker',
+
+  /**
+   * 静态资源 CDN 地址
+   * 用于加速 `assets` 目录中的资源访问
+   */
+  assets_cdn: 'https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io@master/assets/',
+
+  // 本地测试时打开，否则访问的是线上的
+  // assets_cdn: 'assets/',
+
+  // 首页路径
+  index_path: 'index_v3.html',
+
+  // 支持 CORS 的站点列表（实验中...）
+  direct_host_list: 'cors_v1.txt',
+
+  /**
+   * 自定义注入页面的 HTML
+   */
+  inject_html: '<!-- custom html -->',
+
+  /**
+   * URL 自定义处理（设计中）
+   */
+  url_handler: {
+    'https://www.baidu.com/img/baidu_resultlogo@2.png': {
+      replace: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png'
+    },
+    'https://www.pornhub.com/': {
+      redir: 'https://php.net/'
+    },
+    'http://haha.com/': {
+      content: 'Hello World'
+    },
+  }
+})
